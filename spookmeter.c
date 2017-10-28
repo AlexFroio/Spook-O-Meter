981aa8d10b5b68900cd7094eb9706acaf375c221
Derek Witt <derekw023@gmail.com>
2017-10-28 16:02:26 -0400

    Fix timing by using a crystal clock. Also Add auto advance parameter (set to 10 seconds for demo)

--- spookmeter.c
+++ spookmeter.c
@@ -7,75 +7,90 @@
 #define LED5 0x20
 #define BTN 0x08
 
-int i = 4;
-int down = 1;
-int btnPush = 0;
-unsigned int cycle = 20000;
-//int crntLED[] = {LED1, LED2, LED3, LED4, LED5};
-int crntLED[] = {LED1, LED1, LED1, LED1, LED1};
-int delay_per_led[] = {1, 8, 4, 2, 1};
-volatile int state = 1;
-volatile int flag = 0;
-volatile unsigned int debounce=0;
-volatile unsigned int delay_flag = 0;
+//Parameters
+#define SECONDS_TO_ADVANCE 5
 
-void hsdelay (unsigned int);
+int i = 0;
+int down = 1;
+int crntLED[] = {LED1, LED2, LED3, LED4, LED5};
+int delay_per_led[] = {1, 8, 4, 2, 1};
+volatile unsigned int seconds = 0;
+
+void qsdelay (unsigned int time);
 
 void main(void) {
 	WDTCTL = WDTPW + WDTHOLD;
-	DCOCTL = CALDCO_1MHZ;
-	BCSCTL1	= CALBC1_1MHZ;
 
+	// Timing crystal setup
+	BCSCTL1 &= ~XT2OFF; // turn on the LFXT2 osc. (watch crystal)
+	BCSCTL2 |= SELS; // Source SMCLK from crystal
+	BCSCTL3 |= XCAP_3; //Capacitance for watch crystal
+
+	// Output setup
 	P1DIR = LED1 + LED2 + LED3 + LED4 + LED5;  //00110111
 	P1OUT = 0;
 
+	// Enable button interrupt and input settings
 	P1REN = BTN;
-
 	P1IES &= ~BTN;
 	P1IE  = BTN;
+
+	// GIE
 	_enable_interrupts();
 
+	// Hourly advance setup (Setup second-ly timer and do hour check in ISR)
+	TA0CCR0 = 32768;//1 second
+	TA0CTL = TASSEL_1 + MC_1 + TACLR + TAIE;
+
+	// Quarter second long timer for flash patterns
+	TA1CCR0 = 8192;// 1/4 of a second
+	TA1CTL = TASSEL_1 + MC_1 + TACLR + TAIE;
+
+	// Main display loop
 	while (1){
 		P1OUT |= crntLED[i];
-		hsdelay(delay_per_led[i]);
+		qsdelay(delay_per_led[i]);
 		P1OUT &= ~(LED1 + LED2 + LED3 + LED4 + LED5);
-		if (i) hsdelay(delay_per_led[i]);
+		if (i) qsdelay(delay_per_led[i]);
 	}
-
-
 }
 
-void hsdelay(unsigned int time){
-	TA1CCR0 = 1000;
-	TA1CTL = TASSEL_1 + MC_1 + TACLR + TAIE;
-	for (unsigned int time_left=time; time_left; time_left--){
-		for (unsigned int delay_time=250; delay_time; delay_time--){
-			delay_flag = 1;
-			while (delay_flag);
-		}
-		__no_operation();
+// Wait for a specified number of quarter seconds, depend on timer ISR to wakeup every 1/4
+//+second
+void qsdelay(unsigned int time){
+	for(;time; time--){
+		LPM0; //Wait
 	}
-	TA1CTL = 0;		
 	return;
 }
 
+// Advance the meter if button is pressed
 void __attribute__ ((interrupt(PORT1_VECTOR))) btnPressed(void){
-	flag = 1;
-	if (down == 0) i++;
-	else i--;
-
-
 	if (i == 0) down = 0;
 	else if (i == 4) down = 1;
+	if (down == 0) i++;
+	else i--;
 }
 
-
-
-// Millisecond Timer
+// quarter-second Timer
 void __attribute__ ((interrupt(TIMER1_A1_VECTOR))) delay_isr(void){
 	if (TA1IV == 0x0A){
-		delay_flag = 0;
+		LPM0_EXIT;// wakeup
 		return;
 	}
 }
 
+// Second timer, also advance if over a defined threshold
+void __attribute__ ((interrupt(TIMER0_A1_VECTOR))) hour_isr(void){
+	if (TAIV == 0x0A){
+		if (seconds >= SECONDS_TO_ADVANCE){
+			seconds = 0;
+			if (i == 0) down = 0;
+			else if (i >= 4) down = 1;
+			if (down == 0) i++;
+			else i--;
+		}
+		else seconds++;
+	}
+	return;
+}
