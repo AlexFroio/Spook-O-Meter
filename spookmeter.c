07a25e95fca0a7b410d8a6b8ab52e8893f7f83a1
Derek Witt <derekw023@gmail.com>
2017-10-28 16:40:53 -0400

    Fix button interrupt and debounce, accept at most one press every quarter second. Also bias display patterns to be on longer for better readability

--- spookmeter.c
+++ spookmeter.c
@@ -8,13 +8,14 @@
 #define BTN 0x08
 
 //Parameters
-#define SECONDS_TO_ADVANCE 5
+#define SECONDS_TO_ADVANCE 3600 //3600 for one hour
 
 int i = 0;
-int down = 1;
+int down = 0;
 int crntLED[] = {LED1, LED2, LED3, LED4, LED5};
-int delay_per_led[] = {1, 8, 4, 2, 1};
+int delay_per_led[] = {4, 32, 16, 8, 4};
 volatile unsigned int seconds = 0;
+volatile unsigned int pressed = 0;
 
 void qsdelay (unsigned int time);
 
@@ -31,9 +32,10 @@ void main(void) {
 	P1OUT = 0;
 
 	// Enable button interrupt and input settings
-	P1REN = BTN;
+	P1REN |= BTN;
+	P1OUT |= BTN;
 	P1IES &= ~BTN;
-	P1IE  = BTN;
+	P1IE  |= BTN;
 
 	// GIE
 	_enable_interrupts();
@@ -43,15 +45,23 @@ void main(void) {
 	TA0CTL = TASSEL_1 + MC_1 + TACLR + TAIE;
 
 	// Quarter second long timer for flash patterns
-	TA1CCR0 = 8192;// 1/4 of a second
+	TA1CCR0 = 2048;// 1/16 of a second
 	TA1CTL = TASSEL_1 + MC_1 + TACLR + TAIE;
+
+	// Init before enter loop, wait for things to settle and then start. 
+	qsdelay(4); // 1 second is an eternity
+	// Reset state variables
+	i = 0;
+	down = 0; // Not really needed
+	seconds = 0;
+	pressed = 0; // Also maybe not needed
 
 	// Main display loop
 	while (1){
 		P1OUT |= crntLED[i];
-		qsdelay(delay_per_led[i]);
+		qsdelay(delay_per_led[i]+3);
 		P1OUT &= ~(LED1 + LED2 + LED3 + LED4 + LED5);
-		if (i) qsdelay(delay_per_led[i]);
+		if (i) qsdelay(delay_per_led[i]-3);
 	}
 }
 
@@ -66,16 +76,23 @@ void qsdelay(unsigned int time){
 
 // Advance the meter if button is pressed
 void __attribute__ ((interrupt(PORT1_VECTOR))) btnPressed(void){
-	if (i == 0) down = 0;
-	else if (i == 4) down = 1;
-	if (down == 0) i++;
-	else i--;
+	P1IFG &= ~BTN;
+	pressed = 1;
 }
 
 // quarter-second Timer
 void __attribute__ ((interrupt(TIMER1_A1_VECTOR))) delay_isr(void){
 	if (TA1IV == 0x0A){
 		LPM0_EXIT;// wakeup
+		// Button code, debounce by only checking button status once a quarter second
+		if (pressed == 1){
+			pressed++;
+			if (i == 0) down = 0;
+			else if (i >= 4) down = 1;
+			if (down == 0) i++;
+			else i--;
+		}	
+		else if (pressed >= 2) pressed = 0; // Only take at most one press every half second
 		return;
 	}
 }
