511c8e41f0e50934b932543883c88a5b76c7993d
Derek Witt <derekw023@gmail.com>
2017-10-27 23:51:34 -0400

    Cleanup and correct interrupt syntax

--- spookmeter.c
+++ spookmeter.c
@@ -11,7 +11,9 @@ int i = 4;
 int down = 1;
 int btnPush = 0;
 unsigned int cycle = 20000;
-int crntLED[] = {0x01, 0x02, 0x04, 0x10, 0x20};
+//int crntLED[] = {LED1, LED2, LED3, LED4, LED5};
+int crntLED[] = {LED1, LED1, LED1, LED1, LED1};
+int delay_per_led[] = {1, 8, 4, 2, 1};
 volatile int state = 1;
 volatile int flag = 0;
 volatile unsigned int debounce=0;
@@ -24,80 +26,40 @@ void main(void) {
 	DCOCTL = CALDCO_1MHZ;
 	BCSCTL1	= CALBC1_1MHZ;
 
-	//pinMode(2,OUTPUT);
-	//pinMode(3,OUTPUT);
-	//pinMode(4,OUTPUT);
-	//pinMode(6,OUTPUT);
-	//pinMode(7,OUTPUT);
 	P1DIR = LED1 + LED2 + LED3 + LED4 + LED5;  //00110111
 	P1OUT = 0;
 
-	//pinMode(5,INPUT_PULLUP);
 	P1REN = BTN;
 
-	//attachInterrupt(5, btnPressed, RISING);
 	P1IES &= ~BTN;
 	P1IE  = BTN;
-	//	_enable_interrupts();
+	_enable_interrupts();
 
-	//if ( i == 0){
-	//	digitalWrite(2,HIGH);
-	//	delay(cycle);
-	//	digitalWrite(2,LOW);
-	//}
-	//else if (i == 1){
-	//	digitalWrite(3,HIGH);
-	//	delay(cycle*4);
-	//	digitalWrite(3,LOW);
-	//	delay(cycle*4);
-	//}
-	//else if (i == 2){
-	//	digitalWrite(4,HIGH);
-	//	delay(cycle*2);
-	//	digitalWrite(4,LOW);
-	//	delay(cycle*2);
-	//}
-	//else if (i == 3){
-	//	digitalWrite(6,HIGH);
-	//	delay(cycle);
-	//	digitalWrite(6,LOW);
-	//	delay(cycle);
-	//}
-	//else if (i == 4){
-	//	digitalWrite(7,HIGH);
-	//	delay(cycle/2);
-	//	digitalWrite(7,LOW);
-	//	delay(cycle/2);
-	//}
 	while (1){
 		P1OUT |= crntLED[i];
-		hsdelay(1);
+		hsdelay(delay_per_led[i]);
 		P1OUT &= ~(LED1 + LED2 + LED3 + LED4 + LED5);
-		hsdelay(1);
+		if (i) hsdelay(delay_per_led[i]);
 	}
 
 
 }
 
 void hsdelay(unsigned int time){
-//	volatile unsigned int delay_time = time;
-	unsigned int delay_time;
-	//	TA1CCR0 = 1000;
-	//TA1CTL = TASSEL_1 + MC_1 + TACLR + TAIE;
-	for (; time > 0; time--){
-		delay_time=cycle;
-		while (delay_time > 0){
-			//		delay_flag = 1;
-			//		LPM0;
-			delay_time--;
+	TA1CCR0 = 1000;
+	TA1CTL = TASSEL_1 + MC_1 + TACLR + TAIE;
+	for (unsigned int time_left=time; time_left; time_left--){
+		for (unsigned int delay_time=250; delay_time; delay_time--){
+			delay_flag = 1;
+			while (delay_flag);
 		}
+		__no_operation();
 	}
-	//	TA1CTL = 0;		
+	TA1CTL = 0;		
 	return;
 }
 
-#pragma vector=PORT1_VECTOR
-__interrupt void btnPressed(void){
+void __attribute__ ((interrupt(PORT1_VECTOR))) btnPressed(void){
 	flag = 1;
 	if (down == 0) i++;
 	else i--;
@@ -110,11 +72,9 @@ __interrupt void btnPressed(void){
 
 
 // Millisecond Timer
-#pragma vector=TIMER1_A1_VECTOR
-__interrupt void delay_isr(void){
+void __attribute__ ((interrupt(TIMER1_A1_VECTOR))) delay_isr(void){
 	if (TA1IV == 0x0A){
-		//	delay_flag = 0;
-		LPM0_EXIT;
+		delay_flag = 0;
 		return;
 	}
 }
